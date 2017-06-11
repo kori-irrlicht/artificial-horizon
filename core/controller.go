f704baf6bdc0e67f4fd609a183d106bc3a29e997
kori-irrlicht <kori.irrlicht.dev@gmail.com>
2017-06-11 22:26:40 +0200

    Add key mapping

--- core/controller.go
+++ core/controller.go
@@ -1,6 +1,10 @@
 package core
 
-import "github.com/goxjs/glfw"
+import (
+	"errors"
+
+	"github.com/goxjs/glfw"
+)
 
 type AbstractKey int
 
@@ -14,12 +18,24 @@ func (kc *keyboardController) IsDown(ak AbstractKey) bool {
 	return false
 }
 
-func NewKeyboardController(kcm KeyCallbackManager) Controller {
+func NewKeyboardController(kcm KeyCallbackManager, mapping KeyboardMapping) (Controller, error) {
+	if kcm == nil || mapping == nil {
+		return nil, errors.New("Can't pass 'nil' to NewKeyboardController")
+	}
 	kc := &keyboardController{}
 
 	kcm.AddKeyCallback(func(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) bool {
 		return false
 	})
-	return kc
+	return kc, nil
 
 }
+
+// KeyMapping maps a key on the keyboard to a logical key
+type KeyMapping struct {
+	Abstract AbstractKey
+	Key      glfw.Key
+}
+
+// KeyboardMapping is a collection of KeyMappings
+type KeyboardMapping []KeyMapping
--- core/controller_test.go
+++ core/controller_test.go
@@ -3,6 +3,7 @@ package core
 import (
 	"testing"
 
+	"github.com/goxjs/glfw"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -15,14 +16,41 @@ func (tkcm *testKeyCallbackManager) AddKeyCallback(KeyCallback) error {
 	return nil
 }
 
+const testKey = 4
+
 func TestController(t *testing.T) {
 	Convey("Creating a new KeyboardController", t, func() {
 		kcm := &testKeyCallbackManager{}
-		kc := NewKeyboardController(kcm)
+		mapping := KeyboardMapping{{
+			testKey, glfw.Key4,
+		}}
+		kc, err := NewKeyboardController(kcm, mapping)
 		So(kc, ShouldNotBeNil)
+		So(err, ShouldBeNil)
 
 		Convey("It registers a new keycallback listener", func() {
 			So(kcm.registered, ShouldBeTrue)
 		})
+	})
+	Convey("Creating a new KeyboardController with invalid input", t, func() {
+		Convey("Manager and nil", func() {
+			kc, err := NewKeyboardController(&testKeyCallbackManager{}, nil)
+			So(kc, ShouldBeNil)
+			So(err, ShouldNotBeNil)
+
+		})
+		Convey("nil and Keymapping", func() {
+			kc, err := NewKeyboardController(nil, make(KeyboardMapping, 0))
+			So(kc, ShouldBeNil)
+			So(err, ShouldNotBeNil)
+
+		})
+		Convey("nil and nil", func() {
+			kc, err := NewKeyboardController(nil, nil)
+			So(kc, ShouldBeNil)
+			So(err, ShouldNotBeNil)
+
+		})
+
 	})
 }
