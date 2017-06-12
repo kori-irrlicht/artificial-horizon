09ea369d261f19963418a734293487426b35e1ea
kori-irrlicht <kori.irrlicht.dev@gmail.com>
2017-06-12 18:47:14 +0200

    Add TODO note

--- network/channel.go
+++ network/channel.go
@@ -7,6 +7,10 @@ import (
 	"time"
 )
 
+/*
+	// TODO: Implement later. currently not needed
+*/
+
 // channelConn is a channel-based connection
 // It can only be used, if client and server are running in the same process
 type channelConn struct{}
@@ -27,9 +31,7 @@ var newChannelConnections = make(chan *channelConn, 255)
 
 // DialChannel creates a new channel-based connection
 func DialChannel() (net.Conn, error) {
-	cc := &channelConn{}
-
-	return cc, nil
+	return nil, nil
 }
 
 // ListenChannel listens, if a new channel-based connection has been created`
